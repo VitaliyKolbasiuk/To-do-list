b9144f8635e2ad7b628f8d44030fe85b90158169
Vitaliy Kolbasiuk <vitaliykolbasiuk05@gmail.com>
2023-09-11 23:25:02 +0300

    Implementation of the main functionality

---
+++ to_do_list/Todo.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Todo {
+    int m_todo_id = 1;
+    std::string m_todo_name;
+    std::string m_todo_description;
+
+public:
+    static void add_todo(std::vector<Todo>& todo_list, std::string name, std::string description) {
+        Todo todo;
+        if (!todo_list.empty()) todo.m_todo_id = todo_list.back().m_todo_id + 1;
+        todo.m_todo_name = std::move(name);
+        todo.m_todo_description = std::move(description);
+
+        todo_list.emplace_back(todo);
+    }
+
+    static void edit_todo(std::vector<Todo>& todo_list, int todo_id, std::string name, std::string description) {
+        todo_list[todo_id - 1].m_todo_name = std::move(name);
+        todo_list[todo_id - 1].m_todo_description = std::move(description);
+    }
+
+    static void edit_todo_name(std::vector<Todo>& todo_list, int todo_id, std::string name) {
+        todo_list[todo_id - 1].m_todo_name = std::move(name);
+    }
+
+    static void edit_todo_description(std::vector<Todo>& todo_list, int todo_id, std::string description) {
+        todo_list[todo_id - 1].m_todo_description = std::move(description);
+    }
+
+    static void remove_todo(std::vector<Todo>& todo_list, int todo_id) {
+        todo_list.erase(todo_list.cbegin() + todo_id - 1);
+    }
+
+    static void show_todos(std::vector<Todo>& todo_list) {
+        if (todo_list.empty()) {
+            std::cout << "There are no todos" << std::endl;
+        }
+        else {
+            std::cout << "Todo id" << " | " << "Todo name" << " | " << "Todo description" << std::endl;
+            for (const auto& todo : todo_list) {
+                std::cout << todo.m_todo_id << " | " << todo.m_todo_name << " | " << todo.m_todo_description << '\n';
+            }
+        }
+    }
+};
---
+++ to_do_list/main.cpp
@@ -0,0 +1,90 @@
+#include "Todo.h"
+
+int getUserChoice() {
+    int user_action;
+    std::cout << std::endl << "Choose action:" << std::endl;
+    std::cout << "1. Show all todos" << std::endl;
+    std::cout << "2. Add todo" << std::endl;
+    std::cout << "3. Edit todo" << std::endl;
+    std::cout << "4. Remove todo" << std::endl;
+    std::cout << "5. Stop program" << std::endl;
+    std::cin >> user_action;
+    return user_action;
+}
+
+int main() {
+    std::vector<Todo> todo_list;
+
+    while (true) {
+        int user_action_two;
+
+
+        std::string name;
+        std::string description;
+        int todo_id;
+
+        switch (getUserChoice()) {
+        case 1:
+            Todo::show_todos(todo_list);
+            break;
+        case 2:
+            std::cout << "Write name of your todo:" << std::endl;
+            std::cin.ignore();
+            std::getline(std::cin, name);
+            std::cout << "Write description of your todo:" << std::endl;
+            std::getline(std::cin, description);
+            Todo::add_todo(todo_list, name, description);
+            break;
+        case 3:
+            std::cout << "1. Edit name" << std::endl;
+            std::cout << "2. Edit description" << std::endl;
+            std::cout << "3. Edit name and description" << std::endl;
+
+            std::cin >> user_action_two;
+
+            std::cout << "Enter todo id" << std::endl;
+            std::cin >> todo_id;
+
+
+            switch (user_action_two) {
+            case 1:
+                std::cout << "Enter new todo name" << std::endl;
+                std::cin.ignore();
+                std::getline(std::cin, name);
+                Todo::edit_todo_name(todo_list, todo_id, name);
+                break;
+            case 2:
+                std::cout << "Enter new todo description" << std::endl;
+                std::cin.ignore();
+                std::getline(std::cin, description);
+                Todo::edit_todo_description(todo_list, todo_id, description);
+                break;
+            case 3:
+                std::cout << "Enter new todo name" << std::endl;
+                std::cin.ignore();
+                std::getline(std::cin, name);
+                std::cout << "Enter new todo description" << std::endl;
+                std::getline(std::cin, description);
+                Todo::edit_todo(todo_list, todo_id, name, description);
+                break;
+            default:
+                std::cout << "You entered wrong number" << std::endl;
+                break;
+            }
+            break;
+
+        case 4:
+            std::cout << "Enter todo id" << std::endl;
+            std::cin >> todo_id;
+
+            Todo::remove_todo(todo_list, todo_id);
+            break;
+
+        case 5:
+            return 0;
+        default:
+            std::cout << "You entered wrong number" << std::endl;
+            break;
+        }
+    }
+}
